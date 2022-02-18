802214eaa9c04456aab9493cef0ceff04d5ba8c5
Tatiane A. Nicchetti <tanicchetti@gmail.com>
2022-02-18 16:04:22 -0300

    n participants

--- 08 - logica de programacao/A2 - cadastro de eventos/cadastro_eventos.js
+++ 08 - logica de programacao/A2 - cadastro de eventos/cadastro_eventos.js
@@ -7,39 +7,63 @@ class lista_eventos {
 
 
 function criaEvent() {
-    var name = document.getElementById("cevent").value;
+    var name = getValueFromForm ("criaEvent");
     let nuev_evento = new lista_eventos(name);
  
     document.getElementById("cria-event-result").innerHTML = nuev_evento._name;
 }
 
-
-
+//function participante {}
 
 // -------------------------------------------------------
 
 
-let participante = ["a", "b", "c"];
+
+
+
+let evento1 = ["a", "b", "c"];
+let evento2 = ["teste1", "teste2"];
+let evento3 = ["as", "bs", "cs", "ds"];
+evento1.push("ha");
+
+function getValueFromForm (d){
+    let dados = document.getElementById(`${d}`).value;
+    return dados;
+} 
 
 function getEvent () {
-    const event = document.getElementById("event").value;
-
+    const event = getValueFromForm ("event");
+   
     switch (event) {
         case "um": 
-            document.getElementById("event-result").innerHTML =`participantes (${participante.length}): ` + participante;
+            document.getElementById("event-part").innerHTML = evento1;
+            getParticipanteNum (evento1.length);
             break;
+
         case "dois":
-            alert("dois, 1, 2, 3");
+            document.getElementById("event-part").innerHTML = evento2;
+            getParticipanteNum (evento2.length);
             break;
+
         case "três":
-            alert("três, 1, 2 e 3");
+            document.getElementById("event-part").innerHTML = evento3;
+            getParticipanteNum (evento3.length);
             break;
+    }
+}
+
+function getParticipanteNum(n_part) {
+
+    if (n_part >= 100) {
+        document.getElementById("n-part").innerHTML = "número máximo de participantes atingido";
+    } else {
+        document.getElementById("n-part").innerHTML =`nº de participantes: ${n_part}. Ainda é posível cadastrar `
     }
 }
 
 
 function getDate(d) {
-    const date = new Date(document.getElementById(d).value);
+    const date = new Date(getValueFromForm (d));
     const today = new Date();
     return (today.getTime() - date.getTime()); //returns time in ms
 }
@@ -48,9 +72,9 @@ function diffDate() {
     const diff = getDate("date-event");
 
     if (diff < 0) {
-        document.getElementById("date-event-result").innerHTML = "you can upload the event";
+        document.getElementById("date-event-result").innerHTML = "Data válida";
     } else {
-        document.getElementById("date-event-result").innerHTML = "The time has already passed";
+        document.getElementById("date-event-result").innerHTML = "Data inválida!";
        
     }
 }
