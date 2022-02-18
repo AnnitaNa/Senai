044a37a442cc9d187d5a335921da923f00ded2ea
Tatiane A. Nicchetti <tanicchetti@gmail.com>
2022-02-18 11:05:01 -0300

    age and select

--- 08 - logica de programacao/A2 - cadastro de eventos/cadastro_eventos.js
+++ 08 - logica de programacao/A2 - cadastro de eventos/cadastro_eventos.js
@@ -1,16 +1,35 @@
 
-function getDate() {
-    const date_event = new Date(document.querySelector("#data").value);
-    const today = new Date();
-    const diff = date_event.getTime() - today.getTime();
+function getEvent () {
+    const event = document.getElementById("event").select.options[select.selectIndex].value;
+    alert(event);
+}
 
-    
-    if (diff >0) {
-        
-        document.getElementById("data-result").innerHTML = "you can upload the event";
+
+function getDate(d) {
+    const date = new Date(document.getElementById(d).value);
+    const today = new Date();
+    return (today.getTime() - date.getTime()); //returns time in ms
+}
+
+function diffDate() {
+    const diff = getDate("date-event");
+
+    if (diff < 0) {
+        document.getElementById("date-event-result").innerHTML = "you can upload the event";
     } else {
-        document.getElementById("data-result").innerHTML = "The time has already passed";
+        document.getElementById("date-event-result").innerHTML = "The time has already passed";
        
+    }
+}
+
+function getAge () {
+    const age_ms = getDate("date-birth");
+    const age_year = parseInt(age_ms/(1000*60*60*24*365)); //converts date from ms to years and into an integer
+
+    if (age_year < 18) {
+        document.getElementById("date-birth-result").innerHTML = "A pessoa não pode participar por ser menor de idade";
+    } else {
+        document.getElementById("date-birth-result").innerHTML = "participante apto";
     }
 
 }
