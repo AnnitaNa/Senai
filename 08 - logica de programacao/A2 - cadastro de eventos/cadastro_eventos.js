13bfe2da6833da400f9e8da7e1d41a761625701c
Tatiane A. Nicchetti <tanicchetti@gmail.com>
2022-02-22 18:00:31 -0300

    melhoras na organização

--- 08 - logica de programacao/A2 - cadastro de eventos/cadastro_eventos.js
+++ 08 - logica de programacao/A2 - cadastro de eventos/cadastro_eventos.js
@@ -1,79 +1,24 @@
+//******************************************************************************************************
+//**********************************  CADASTRO DE EVENTOS (FORMULÁRIO) *********************************
+//******************************************************************************************************
+//************************ by: Tatiane A. Nicchetti | tanicchetti@hotmail.com **************************
+//******************************************************************************************************
+//
+//
+// criar formulário para cadastro de eventos. 
+//Requisitos: Ver participante por evento. Participantes > 18 anos e com limite máximo de pessoas
 
 
 
+// ----------------------  FUNÇÕES GERAIS ------------------------
 
-// function to get value from the html form
-
+//retorna valor do formulário
 function getValueFromForm (d){
-    let dados = document.getElementById(`${d}`).value;
-    return dados;
+    let valor = document.getElementById(`${d}`).value;
+    return valor;
 } 
 
-
-
-function getMembers () {
-    const event = getValueFromForm ("event");
-    let index = lista_eventos.all.indexOf(event);
-    let members = arr_event[index].participante;
-    return members
-    } 
-
-
-
-
-// see if the maximum number of members was reached
-
-function memberLimit(n) {
-
-    if (n >= 5) {
-        document.getElementById("member-result").innerHTML = `Não é mais possível cadastrar! <br>Número máximo de participantes atingido nesse evento!`;
-        exit;
-    }
-}
-
-
-
-
-//get the age
-function getAge () {
-    const age_ms = getDate("date-birth");
-    const age_year = parseInt(age_ms/(1000*60*60*24*365)); //converts date from ms to years and into an integer
-
-    if (age_year > 18) {
-        return
-    } else {
-        document.getElementById("member-result").innerHTML = "idade não é válida. Mín de 18 anos";
-        exit
-    }
-}
-
-//see all conditions to add a new member to the event
-function participante() {
-    let member = getMembers ();
-
-    memberLimit (member.length);
-    getAge();
-   
-    let name = getValueFromForm ("member");
-    if (validateEl (member,name)) {
-        document.getElementById("member-result").innerHTML = ` ${name} já é membro desse evento ou nome é inválido`;
-        exit;
-    }
-    
-
-    member.push(name);
-    document.getElementById("member-result").innerHTML = `${name} foi adicionado ao evento. <br> Agora há ${member.length} participantes`;
-    
-}
-
-function verParticipantes() {
-    let participantes = getMembers();
-    document.getElementById("total-members").innerHTML = `participantes: ${participantes}`
-}
-
-
-
-//vê se el está contido em arr ou se el é vazio
+//vê se elemento está contido em uma array ou se ele é vazio
 function validateEl(arr, el) {
     if(arr.includes(`${el}`) || el=="") {
         return true;
@@ -82,15 +27,19 @@ function validateEl(arr, el) {
     }
 }
 
+// ----------------------- NOVO EVENTO ----------------------------
 
-// get the difference between a given date and today (in miliseconds)
+
+//        --------    1º) restrição da data     -------
+
+// diferença entre uma data e hoje (em ms)
 function getDate(d) {
     const date = new Date(getValueFromForm (d));
     const today = new Date();
     return (today.getTime() - date.getTime()); //returns time in ms
 }
 
-// discover if the date is after today or not
+// descobre se uma data é posterior a hoje
 function diffDate() {
     const diff = getDate("date-event");
 
@@ -103,30 +52,28 @@ function diffDate() {
 }
 
 
-//------------------------------------- NOVO EVENTO
+//         --------   2º) adicionar nome do novo elemento   ----------
 
-//classe
+// fabrica de classes para armazenar eventos
 class lista_eventos  {
     constructor (evento) {
         this.evento=evento;
         this.participante=[];
-        lista_eventos.all.push(evento); //to log all events on the static all
+        lista_eventos.all.push(evento); 
     }
-    static all = [];
+    static all = []; //salva todos os eventos com essa classe
 }
 
-
-let arr_event = [];
-
 //classes padrão (exemplos)
-
-
 let Carnaval = new lista_eventos("Carnaval");
 Carnaval.participante.push("Amanda","Beatriz", "Catia")
 
-
+//array que salva todos os objetos da classe lista_eventos
+let arr_event = [];
 arr_event.push(Carnaval);
 
+
+// função para criar um evento
 function criaEvent() {
 
     diffDate();
@@ -136,14 +83,87 @@ function criaEvent() {
         document.getElementById("cria-event-result").innerHTML = "Esse evento já existe ou nome é inválido";
     } else {
         var a = new lista_eventos(newEvent);
-        arr_event.push(a);
+        arr_event.push(a);  //adiciona objeto a arr de eventos
        
-        // includes the element in the list
+        // inclui o novo elemento na lista de "escolha seu evento"
         var option = document.createElement("option");
         option.text = `${newEvent}`;
         option.value=`${newEvent}`;
         document.getElementById("event").add(option);
-        document.getElementById("cria-event-result").innerHTML = `Evento "${newEvent}" incluido. Todos os eventos: ${lista_eventos.all}`
+        document.getElementById("cria-event-result").innerHTML = `Evento "${newEvent}" incluido`
     }
 }
+
+
+
+
+
+// ----------------------  PARTICIPANTES ------------------
+
+
+//mostrar os aprticipantes de certo evento
+function verParticipantes() {
+    let participantes = getMembers();
+    document.getElementById("total-members").innerHTML = `participantes(${participantes.length}): ${participantes} `
+}
+
+
+//vê se todas as condições foram atingidas para add o participantes
+function participante() {
+    let member = getMembers ();
+    memberLimit (member.length);
+    getAge();
+    let name = getValueFromForm ("member");
+
+    if (validateEl (member,name)) {
+        document.getElementById("member-result").innerHTML = ` ${name} já é membro desse evento ou nome é inválido`;
+        exit;
+    }
+    
+    member.push(name);
+    document.getElementById("member-result").innerHTML = `${name} foi adicionado ao evento. <br> Agora há ${member.length} participantes`;
+}
+
+//localiza os participantes do objeto evento
+//para isso, acha o index do objeto correspondente na arr com todos os objetos
+function getMembers () {
+    const event = getValueFromForm ("event");
+    let index = lista_eventos.all.indexOf(event);
+    let members = arr_event[index].participante;
+    return members
+    } 
+
+// confere se nº max de participantes foi atingido
+function memberLimit(n) {
+
+    if (n >= 5) {
+        document.getElementById("member-result").innerHTML = `Não é mais possível cadastrar! <br>Número máximo de participantes atingido nesse evento!`;
+        exit;
+    }
+}
+
+
+//Vê a idade do participante e se ele é >18 anos
+function getAge () {
+    const age_ms = getDate("date-birth");
+    const age_year = parseInt(age_ms/(1000*60*60*24*365)); //converts date from ms to years and into an integer
+
+    if (age_year > 18) {
+        return
+    } else {
+        document.getElementById("member-result").innerHTML = "idade não é válida. Mín de 18 anos";
+        exit
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
 
