35b816191fda8a349e9be55e1ef972e22364f276
Tatiane A. Nicchetti <tanicchetti@gmail.com>
2022-02-22 17:12:40 -0300

    funcionando!

--- 08 - logica de programacao/A2 - cadastro de eventos/cadastro_eventos.js
+++ 08 - logica de programacao/A2 - cadastro de eventos/cadastro_eventos.js
@@ -1,11 +1,5 @@
 
 
-// contst declaration (events)
-
-let evento1 = ["a", "b", "c"];
-let evento2 = ["teste1", "teste2"];
-let evento3 = ["as", "bs", "cs", "ds"];
-
 
 
 // function to get value from the html form
@@ -15,28 +9,16 @@ function getValueFromForm (d){
     return dados;
 } 
 
-//function to get the event by calling the switch function
 
-function getEvent () {
+
+function getMembers () {
     const event = getValueFromForm ("event");
-    const theEvent = whichEvent(event);
-    document.getElementById("event-part").innerHTML = theEvent;
-}
+    let index = lista_eventos.all.indexOf(event);
+    let members = arr_event[index].participante;
+    return members
+    } 
 
 
-// discover which event was selected
-function whichEvent (event) {
-    switch (event) {
-        case "Evento 1": 
-            return evento1;
-
-        case "Evento 2": 
-            return evento2;
-
-        case "Evento 3": 
-            return evento3;
-    }
-}
 
 
 // see if the maximum number of members was reached
@@ -67,21 +49,26 @@ function getAge () {
 
 //see all conditions to add a new member to the event
 function participante() {
-    const event = getValueFromForm ("event");
-    const theEvent = whichEvent(event);
+    let member = getMembers ();
 
-    memberLimit (theEvent.length);
+    memberLimit (member.length);
     getAge();
    
     let name = getValueFromForm ("member");
-    if (validateEl (theEvent,name)) {
+    if (validateEl (member,name)) {
         document.getElementById("member-result").innerHTML = ` ${name} já é membro desse evento ou nome é inválido`;
         exit;
     }
     
 
-    theEvent.push(name);
-    document.getElementById("member-result").innerHTML = `${name} foi adicionado a ${event}. <br> Agora há ${theEvent.length} participantes`;
+    member.push(name);
+    document.getElementById("member-result").innerHTML = `${name} foi adicionado ao evento. <br> Agora há ${member.length} participantes`;
+    
+}
+
+function verParticipantes() {
+    let participantes = getMembers();
+    document.getElementById("total-members").innerHTML = `participantes: ${participantes}`
 }
 
 
@@ -115,28 +102,48 @@ function diffDate() {
     }
 }
 
-let list_events = ["a", "b", "c"];
+
+//------------------------------------- NOVO EVENTO
+
+//classe
+class lista_eventos  {
+    constructor (evento) {
+        this.evento=evento;
+        this.participante=[];
+        lista_eventos.all.push(evento); //to log all events on the static all
+    }
+    static all = [];
+}
 
 
+let arr_event = [];
+
+//classes padrão (exemplos)
+
+
+let Carnaval = new lista_eventos("Carnaval");
+Carnaval.participante.push("Amanda","Beatriz", "Catia")
+
+
+arr_event.push(Carnaval);
 
 function criaEvent() {
-     
+
     diffDate();
-    
     var newEvent = getValueFromForm ("criaEvent");
 
-    if (validateEl(list_events,newEvent)) {
+    if (validateEl(lista_eventos.all,newEvent)) {
         document.getElementById("cria-event-result").innerHTML = "Esse evento já existe ou nome é inválido";
-    }   else {
-        list_events.push(`${newEvent}`)
-        document.getElementById("cria-event-result").innerHTML = `Evento "${newEvent}" incluido. Todos os eventos: ${list_events}`;
-
-    
+    } else {
+        var a = new lista_eventos(newEvent);
+        arr_event.push(a);
+       
         // includes the element in the list
         var option = document.createElement("option");
         option.text = `${newEvent}`;
         option.value=`${newEvent}`;
         document.getElementById("event").add(option);
-
+        document.getElementById("cria-event-result").innerHTML = `Evento "${newEvent}" incluido. Todos os eventos: ${lista_eventos.all}`
     }
 }
+
