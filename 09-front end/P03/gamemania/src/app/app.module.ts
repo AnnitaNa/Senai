4747a3936afd303e63d8445216f40d806547d482
Tatiane A. Nicchetti <tanicchetti@gmail.com>
2022-04-26 10:36:49 -0300

    update

--- 09-front end/P03/gamemania/src/app/app.module.ts
+++ 09-front end/P03/gamemania/src/app/app.module.ts
@@ -4,9 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+  
   ],
   imports: [
     BrowserModule,
