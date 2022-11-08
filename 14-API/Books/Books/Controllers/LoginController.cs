262842bdfd7257cfc5de15d4e4838a3a88c1a34d
Tatiane A. Nicchetti <tanicchetti@gmail.com>
2022-11-08 12:03:25 -0300

    FEATURE: login and jwt authentication

---
+++ 14-API/Books/Books/Controllers/LoginController.cs
@@ -0,0 +1,58 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using Books.Interfaces;
+using Books.Models;
+using Books.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using JwtRegisteredClaimNames = Microsoft.IdentityModel.JsonWebTokens.JwtRegisteredClaimNames;
+
+namespace Books.Controllers;
+
+[Produces("application/json")]
+[Route("api/[controller]")]
+[ApiController]
+public class LoginController : Controller
+{
+    private readonly IUserRepository _userRepository;
+
+    public LoginController(IUserRepository userRepository)
+    {
+        _userRepository = userRepository;
+    }
+
+    [HttpPost]
+    public IActionResult Login(LoginViewModel login)
+    {
+        User foundUser = _userRepository.Login(login.Email, login.Senha);
+        if (foundUser == null)
+        {
+            return Unauthorized(new { msg = "Invalid Email or password" });
+        }
+
+        var myClaims = new[]
+        {
+            new Claim(JwtRegisteredClaimNames.Email, foundUser.Email),
+            new Claim(JwtRegisteredClaimNames.Jti, foundUser.Id.ToString()),
+            new Claim(ClaimTypes.Role, foundUser.Tipo)
+        };
+
+        var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes("chapter-chava-auteticacao"));
+
+        var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+        var myToken = new JwtSecurityToken(
+            issuer: "chapter.webp",
+            audience: "chapter.webp",
+            claims: myClaims,
+            signingCredentials: credentials,
+            expires: DateTime.Now.AddMinutes(60)
+        );
+        return Ok(
+            new
+            {
+                token = new JwtSecurityTokenHandler().WriteToken(myToken)
+            }
+        );
+    }
+}
--- 14-API/Books/Books/Program.cs
+++ 14-API/Books/Books/Program.cs
@@ -1,6 +1,7 @@
 using Books.Contexts;
 using Books.Interfaces;
 using Books.Repositories;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,6 +12,28 @@ builder.Services.AddControllers();
 builder.Services.AddScoped<BookContext, BookContext>();
 builder.Services.AddTransient<BookRepository, BookRepository>();
 builder.Services.AddTransient<IUserRepository, UserRepository>();
+
+//no nuget baixar Microsoft.AspeNetCore.Authentication.JwtBerarer
+builder.Services
+    .AddAuthentication(options =>
+    {
+        options.DefaultChallengeScheme = "JwtBearer";
+        options.DefaultAuthenticateScheme = "JwtBearer";
+
+    })
+    .AddJwtBearer("JwtBearer", options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            IssuerSigningKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes("chapter-chava-auteticacao")),
+            ClockSkew = TimeSpan.FromMinutes(60),
+            ValidIssuer = "chapter.webp",
+            ValidAudience = "chapter.webp"
+        };
+    });
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -26,6 +49,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
